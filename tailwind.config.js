4c0e6aef1b4ff9eb8bfa654d681372f395c4c88f
jamesrea1 <jamesreadev@outlook.com>
2021-03-11 05:51:57 +0000

    Config: Tailwind - theme colours, extend spacing with percent classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      twitter: '#1DA1F2',
+      
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      bluegray: colors.blueGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      lightblue: colors.lightBlue
+    },
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+        '1/5': '20%',
+        '2/5': '40%',
+        '3/5': '60%',
+        '4/5': '80%',
+        '1/6': '16.666667%',
+        '2/6': '33.333333%',
+        '3/6': '50%',
+        '4/6': '66.666667%',
+        '5/6': '83.333333%'  
+      }
+    },
   },
   variants: {
     extend: {},
